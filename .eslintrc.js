5b15c3b3e1d6926ffb8f60e5167b071a05e3d9b4
aki <lala.akira@gmail.com>
2021-08-15 11:11:49 +0900

    refactor: use @sonicgarden/eslint-plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,70 +1,14 @@
+// eslint-disable-next-line unicorn/prefer-module
 module.exports = {
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@sonicgarden'],
   extends: [
-    'airbnb-base',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:vue/recommended',
-    'plugin:prettier/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'prettier/vue',
+    'plugin:@sonicgarden/browser',
+    'plugin:@sonicgarden/recommended',
+    'plugin:@sonicgarden/typescript',
+    'plugin:@sonicgarden/vue-typescript',
+    'plugin:@sonicgarden/prettier',
   ],
-  env: {
-    browser: true,
-  },
-  parser: '@typescript-eslint/parser',
   settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
-      },
-    },
-    // eslint-import-resolver-typescript
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
+    'import/internal-regex': '^src/',
   },
-  rules: {
-    // NOTE: Disallow async functions which have no await expression
-    'require-await': 'warn',
-
-    // NOTE: 競合するルール調整
-    // SEE: https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-useless-constructor.md
-    'no-useless-constructor': 'off',
-    '@typescript-eslint/no-useless-constructor': ['error'],
-
-    // SEE: https://engineering.linecorp.com/ja/blog/you-dont-need-default-export/
-    'import/prefer-default-export': 'off',
-
-    'import/extensions': [
-      'error',
-      'always',
-      {
-        ts: 'never',
-        tsx: 'never',
-        js: 'never',
-        jsx: 'never',
-      },
-    ],
-
-    // prettier
-    'prettier/prettier': [
-      'error',
-      {
-        arrowParens: 'always',
-        bracketSpacing: true,
-        printWidth: 100,
-        semi: true,
-        singleQuote: true,
-        trailingComma: 'all',
-        useTabs: false,
-      },
-    ],
-
-    // NOTE: typescript-eslintと競合するルールを無効化
-    // SEE: https://github.com/vuejs/eslint-config-typescript/blob/master/index.js
-    'no-undef': 'off',
-    'no-unused-vars': 'off',
-    strict: 'off',
-  },
-};
+}
